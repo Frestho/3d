b67e34bec4844133901b80a2c1ece1338df73626
Conics <34761944+Frestho@users.noreply.github.com>
2018-02-16 20:28:09 -0800

    Update Main.js

--- Main.js
+++ Main.js
@@ -2,12 +2,12 @@ function setup() {
   createCanvas(600, 600);
 }
 function draw() {
-  background(255);
+  background(240);
   for(var i = 6; i > -1; i--) {
     fill(50, 120, 245);
     stroke(0);
     strokeWeight(2);
     var width = 100-i*2;
-    rect((mouseX-width/2)-(mouseX-300)/((20-i)/8), mouseY-width/2-(mouseY-300)/((20-i)/8), width, width)
+    rect((mouseX-width/2)-(mouseX-300)/((40-i)/20), mouseY-width/2-(mouseY-300)/((40-i)/20), width, width)
   }
 }
